e755d89b8422c1f3a4c0a41cce9be9c6015e9c27
nakashima-akihiro <nakashima@hab-co.jp>
2025-09-09 00:52:50 +0900

    メール内のURLは環境変数から取得

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,7 +1,8 @@
 class OrderMailer < ApplicationMailer
   def complete(args)
     email = args[:email]
-    @url  = 'http://localhost:8000/orders'
+    # 環境変数からホスト名を取得
+    @url  = ENV.fetch('DEFAULT_URL') { 'http://localhost:8000/orders' }
     mail(to: email, subject: 'Your order has been completed')
   end
 end
