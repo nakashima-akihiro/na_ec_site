4e6cf08503471b797d2f36aa1a98aa9139a3f416
akihiro nakashima <nakashima@akihironoMacBook-Air.local>
2026-01-20 14:29:26 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with(
+    name: ENV.fetch('BASIC_AUTH_USERNAME', 'admin'),
+    password: ENV.fetch('BASIC_AUTH_PASSWORD', 'password')
+  )
 end
