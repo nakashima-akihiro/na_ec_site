cd5f87ac9475b33fa2a435c8025a484de591697e
nakashima-akihiro <akihironakashima0@gmail.com>
2026-02-10 23:07:34 +0900

    環境変数でbasicのon/off切り替えられるように

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with(
-    name: ENV.fetch('BASIC_AUTH_USERNAME', 'admin'),
-    password: ENV.fetch('BASIC_AUTH_PASSWORD', 'password')
-  )
+  if ENV.fetch('BASIC_AUTH_ENABLED', 'false') == 'true'
+    http_basic_authenticate_with(
+      name: ENV.fetch('BASIC_AUTH_USERNAME', 'admin'),
+      password: ENV.fetch('BASIC_AUTH_PASSWORD', 'password')
+    )
+  end
 end
