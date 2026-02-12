aaefc56fc543f6693d6075e9e2fc3102e5301db7
nakashima-akihiro <akihironakashima0@gmail.com>
2026-02-13 00:32:31 +0900

    basicオンでもstripe決済できるように

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   if ENV.fetch('BASIC_AUTH_ENABLED', 'false') == 'true'
     http_basic_authenticate_with(
       name: ENV.fetch('BASIC_AUTH_USERNAME', 'admin'),
-      password: ENV.fetch('BASIC_AUTH_PASSWORD', 'password')
+      password: ENV.fetch('BASIC_AUTH_PASSWORD', 'password'),
+      unless: ->(controller) { controller.controller_path == 'customer/webhooks' }
     )
   end
 end
